f02655b9ba34db462889e938bebd09420c8f5525
gyeong <gyeongyeongi22@gmail.com>
2022-05-26 10:57:36 +0900

    first push

---
+++ app/src/main/java/com/corporation8793/mealkit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.corporation8793.mealkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
