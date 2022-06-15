eaf282ffb6d3a87761a4103ebce711fb34d73209
Kim <divingsky@naver.com>
2022-06-15 18:39:17 +0900

    주문/결제 작업중

---
+++ app/src/main/java/com/corporation8793/mealkit/payment/PayMentActivity.kt
@@ -0,0 +1,11 @@
+package com.corporation8793.mealkit.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayMentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_ment)
+    }
+}
