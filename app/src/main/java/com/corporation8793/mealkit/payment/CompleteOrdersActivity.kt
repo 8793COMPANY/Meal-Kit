ee4e2efde311bfa445efa0526098838efd5df7cd
Kim <divingsky@naver.com>
2022-06-20 10:50:32 +0900

    주문 결제 페이지 수정 2

--- app/src/main/java/com/corporation8793/mealkit/payment/CompleteOrdersActivity.kt
+++ app/src/main/java/com/corporation8793/mealkit/payment/CompleteOrdersActivity.kt
@@ -2,10 +2,16 @@ package com.corporation8793.mealkit.payment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.corporation8793.mealkit.R
+import com.corporation8793.mealkit.databinding.ActivityCompleteOrdersBinding
+
 
 class CompleteOrdersActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCompleteOrdersBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_complete_orders)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_complete_orders)
+        binding.setActionBar("주문 완료")
     }
 }
