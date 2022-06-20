e4694db4d19a9712e4596a1eb9e8883605ed46b2
Doo-Dong <ehd6754@gmail.com>
2022-06-20 15:09:32 +0900

    [REFACTOR] Customer.kt

--- app/src/main/kotlin/com/corporation8793/mealkit/esf_wp/rest/repository/BoardRepository.kt
+++ app/src/main/kotlin/com/corporation8793/mealkit/esf_wp/rest/repository/BoardRepository.kt
@@ -1,6 +1,7 @@
 package com.corporation8793.mealkit.esf_wp.rest.repository
 
 import com.corporation8793.mealkit.esf_wp.rest.RestClient
+import com.corporation8793.mealkit.esf_wp.rest.data.Product
 import com.corporation8793.mealkit.esf_wp.rest.data.Store
 import okhttp3.Credentials
 
@@ -13,6 +14,21 @@ import okhttp3.Credentials
  * @see     <a href="https://developer.wordpress.org/rest-api/reference/comments/">WP-Comments [REST API Reference]</a>
  */
 class BoardRepository {
+    /**
+     * 전체 상품을 검색합니다.
+     * @author  두동근
+     * @return  responseCode (expected : "200"), [List<Product>]
+     * @see     Post
+     * @see     Pair
+     * @see     <a href="https://developer.wordpress.org/rest-api/reference/posts/#list-posts">List Posts [REST API Reference]</a>
+     */
+    fun listAllProduct() : Pair<String, List<Product>?> {
+        val call = RestClient.boardService.listAllProduct()
+
+        val response = call.execute()
+
+        return Pair(response.code().toString(), response.body())
+    }
     /**
      * 전체 체인점을 검색합니다.
      * @author  두동근
